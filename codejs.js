5fdd745f9871c67a4a8109f6cc0dd63ac155bc56
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 16:13:07 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,5 +1,6 @@
 // Function to submit the application
-function submitApplication() {
+function submitApplication(event) {
+  event.preventDefault();
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
 
@@ -9,31 +10,28 @@ function submitApplication() {
     jsonObject[key] = value;
   });
 
-  // Créer une requête XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  // Gérer la réponse
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      // Vérifier si la soumission a réussi
-      if (xhr.status === 200) {
-        console.log('Form submitted successfully');
-        // Afficher un message de confirmation si nécessaire
-        alert('Application submitted successfully!');
-        // Fermer la modal après soumission du formulaire
-        toggleModal();
-      } else {
-        console.error('Error submitting application:', xhr.statusText);
-        alert('Failed to submit application. Please try again later.');
-      }
+  // Envoyer les données du formulaire à Google Forms
+  fetch('https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', {
+    method: 'POST',
+    body: new URLSearchParams(jsonObject),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
-  };
-
-  // Convertir les données du formulaire en format query string
-  var formDataQueryString = new URLSearchParams(jsonObject).toString();
-
-  // Envoyer la requête avec les données du formulaire
-  xhr.send(formDataQueryString);
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Form submitted successfully');
+      // Afficher un message de confirmation si nécessaire
+      alert('Application submitted successfully!');
+      // Fermer la modal après soumission du formulaire
+      toggleModal();
+    } else {
+      console.error('Error submitting application:', response.statusText);
+      alert('Failed to submit application. Please try again later.');
+    }
+  })
+  .catch(error => {
+    console.error('Error submitting application:', error);
+    alert('Failed to submit application. Please try again later.');
+  });
 }
