d755b75b371c032b8367c4a733958ed5c4c67263
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 14:55:38 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,48 +1,44 @@
-// Function to toggle modal and apply animation
-function toggleModal() {
-  var modal = document.getElementById("myModal");
-  modal.classList.toggle("active");
+// Importer les fonctionnalités nécessaires depuis le SDK Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-  // Add animation class
-  modal.classList.toggle("slide-in");
-}
+// Configuration de votre application Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCCv2N0CAWq5VXgM1NcERA2Mb0If271eJQ",
+  authDomain: "sazz-b61a4.firebaseapp.com",
+  projectId: "sazz-b61a4",
+  storageBucket: "sazz-b61a4.appspot.com",
+  messagingSenderId: "84222743900",
+  appId: "1:84222743900:web:4195bebaa9a888e86f9d99",
+  measurementId: "G-GD4THY6V85"
+};
 
-// Function to submit the form
+// Initialiser votre application Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Function pour soumettre le formulaire
 function submitForm(event) {
   event.preventDefault();
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
 
-  // Convert form data to JSON object
+  // Convertir les données du formulaire en objet JSON
   var jsonObject = {};
   formData.forEach(function(value, key) {
     jsonObject[key] = value;
   });
 
-  // Nouveau lien de déploiement du service web
-  var url = 'https://script.google.com/macros/s/AKfycbyPf-aNotFliYRlVdtAwuSOZSEag1BTjFeJlhPhDbzO064PZ0eQjSXQ8Ke7Hmyy1oYrlw/exec';
-
-  // Send JSON data to Google Sheets
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(jsonObject),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    // Afficher un message de confirmation
-    alert('Application submitted successfully!');
-    // Attendre 2 secondes avant de fermer la modal
-    setTimeout(function() {
-      toggleModal(); // Close modal after form submission
-    }, 2000);
-  })
-  .catch(error => {
-    console.error('There was an error!', error);
-    alert('Failed to submit application. Please try again later.');
-  });
+  // Ajouter les données au Firestore
+  addDoc(collection(db, "leads"), jsonObject)
+    .then(() => {
+      // Afficher un message de confirmation
+      alert('Application submitted successfully!');
+      // Fermer la modal après soumission du formulaire
+      toggleModal();
+    })
+    .catch((error) => {
+      console.error('Error submitting application:', error);
+      alert('Failed to submit application. Please try again later.');
+    });
 }
