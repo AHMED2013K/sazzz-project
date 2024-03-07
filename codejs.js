1a072ec669e629c165a64490e626db3877fbc75a
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 10:56:52 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,39 +1,22 @@
-// Fonction pour soumettre le formulaire
 function submitForm() {
-    var form = document.getElementById("myForm");
-    var formData = new FormData(form);
-
-    // Remplacer 'https://script.google.com/macros/s/AKfycbzDEP78QP8vhZWNEHimoSzlwACkLZe-rosQHEE5uM8O0OgelI8DtWZRn6zsmRGRZrAD/exec' par l'URL de déploiement correcte
-    var url = 'https://script.google.com/macros/s/AKfycbzDEP78QP8vhZWNEHimoSzlwACkLZe-rosQHEE5uM8O0OgelI8DtWZRn6zsmRGRZrAD/exec';
-
-    // Envoyer les données du formulaire à Google Sheets
-    fetch(url, {
-        method: 'POST',
-        body: formData
+    var formData = new FormData(document.getElementById("myForm"));
+    var data = {};
+    formData.forEach(function(value, key){
+        data[key] = value;
+    });
+    fetch("https://script.google.com/macros/s/AKfycbzDEP78QP8vhZWNEHimoSzlwACkLZe-rosQHEE5uM8O0OgelI8DtWZRn6zsmRGRZrAD/exec", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('La réponse du réseau n\'était pas valide');
-        }
-        // Afficher un message de confirmation
-        alert('Formulaire soumis avec succès!');
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
     })
     .catch(error => {
-        console.error('Une erreur s\'est produite!', error);
-        alert('Échec de la soumission du formulaire. Veuillez réessayer plus tard.');
+        console.error('Erreur :', error);
+        alert('Une erreur s\'est produite lors de l\'envoi du formulaire.');
     });
-}
-
-// Appeler la fonction submitForm() lorsque le bouton d'envoi est cliqué
-document.getElementById("submitBtn").addEventListener("click", submitForm);
-
-// Fonction pour afficher le modal et appliquer l'animation
-function toggleModal() {
-    var modal = document.getElementById("myModal");
-    modal.classList.add("active");
-}
-
-// Appeler la fonction toggleModal() dès que la page est chargée
-window.onload = function() {
-    toggleModal();
 }
