7d0c9d8f29fbf2c893aff57a1b408bc459c23de0
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 11:17:10 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,5 +1,3 @@
-User
-Voici mon code js : 
 // Function to toggle modal and apply animation
 function toggleModal() {
   var modal = document.getElementById("myModal");
@@ -8,3 +6,21 @@ function toggleModal() {
   // Add animation class
   modal.classList.toggle("slide-in");
 }
+
+// Function to submit the form
+function submitForm(event) {
+  event.preventDefault();
+@@ -34,8 +25,12 @@ function submitForm(event) {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Afficher un message de confirmation
+    alert('Application submitted successfully!');
+    toggleModal(); // Close modal after form submission
+    // Attendre 2 secondes avant de fermer la modal
+    setTimeout(function() {
+      toggleModal(); // Close modal after form submission
+    }, 2000);
+  })
+  .catch(error => {
+    console.error('There was an error!', error);
