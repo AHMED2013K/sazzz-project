34381a098cfb2a5d3887d2269bb1287e28489649
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 15:49:54 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,23 +1,3 @@
-// Importer les fonctionnalités nécessaires depuis le SDK Firebase
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-
-// Configuration de votre application Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCCv2N0CAWq5VXgM1NcERA2Mb0If271eJQ",
-  authDomain: "sazz-b61a4.firebaseapp.com",
-  projectId: "sazz-b61a4",
-  databaseURL: "https://sazz-b61a4-default-rtdb.europe-west1.firebasedatabase.app",
-  storageBucket: "sazz-b61a4.appspot.com",
-  messagingSenderId: "84222743900",
-  appId: "1:84222743900:web:4195bebaa9a888e86f9d99",
-  measurementId: "G-GD4THY6V85"
-};
-
-// Initialiser votre application Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 // Function pour soumettre le formulaire
 function submitForm(event) {
   event.preventDefault();
@@ -30,16 +10,14 @@ function submitForm(event) {
     jsonObject[key] = value;
   });
 
-  // Ajouter les données au Firestore
-  addDoc(collection(db, "leads"), jsonObject)
-    .then(() => {
-      // Afficher un message de confirmation
-      alert('Application submitted successfully!');
-      // Fermer la modal après soumission du formulaire
-      toggleModal();
-    })
-    .catch((error) => {
-      console.error('Error submitting application:', error);
-      alert('Failed to submit application. Please try again later.');
-    });
+  // Envoi des données du formulaire au Google Forms (pas nécessaire pour le script Google Apps Script)
+}
+
+// Function to toggle modal and apply animation
+function toggleModal() {
+  var modal = document.getElementById("myModal");
+  modal.classList.toggle("active");
+
+  // Add animation class
+  modal.classList.toggle("slide-in");
 }
