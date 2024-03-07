cb6ac5a99caf36e7d38c9cbe440a39bdeeb318dd
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 11:12:26 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,3 +1,4 @@
+// Fonction pour soumettre le formulaire
 function submitForm() {
     var formData = new FormData(document.getElementById("myForm"));
     var data = {};
@@ -20,3 +21,24 @@ function submitForm() {
         alert('Une erreur s\'est produite lors de l\'envoi du formulaire.');
     });
 }
+
+// Appeler la fonction submitForm() lorsque le bouton d'envoi est cliqué
+document.getElementById("submitBtn").addEventListener("click", submitForm);
+
+// Fonction pour afficher le modal
+function toggleModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Fermer le modal lorsque l'utilisateur clique sur la croix
+document.getElementsByClassName("close")[0].addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
+});
+
+// Fermer le modal lorsque l'utilisateur clique en dehors du modal
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+        document.getElementById("myModal").style.display = "none";
+    }
+};
