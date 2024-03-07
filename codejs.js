612b731d8e0461c5252b1ae89e05aa1840916a97
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 11:24:52 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,26 +1,35 @@
-// Function to toggle modal and apply animation
-function toggleModal() {
-  var modal = document.getElementById("myModal");
-  modal.classList.toggle("active");
-
-  // Add animation class
-  modal.classList.toggle("slide-in");
-}
-
 // Function to submit the form
 function submitForm(event) {
   event.preventDefault();
-@@ -34,8 +25,12 @@ function submitForm(event) {
+  var form = document.getElementById("myForm");
+  var formData = new FormData(form);
+
+  // Convert form data to JSON object
+  var jsonObject = {};
+  formData.forEach(function(value, key) {
+    jsonObject[key] = value;
+  });
+
+  // Replace 'https://api.example.com/sheets' with deployment URL
+  var url = 'https://script.google.com/macros/s/AKfycbzDEP78QP8vhZWNEHimoSzlwACkLZe-rosQHEE5uM8O0OgelI8DtWZRn6zsmRGRZrAD/exec';
+
+  // Send JSON data to Google Sheets
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(jsonObject),
+  })
+  .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    // Afficher un message de confirmation
-    alert('Application submitted successfully!');
+    console.log('Application submitted successfully!'); // Changed from alert to console.log
     toggleModal(); // Close modal after form submission
-    // Attendre 2 secondes avant de fermer la modal
-    setTimeout(function() {
-      toggleModal(); // Close modal after form submission
-    }, 2000);
   })
   .catch(error => {
     console.error('There was an error!', error);
+    alert('Failed to submit application. Please try again later.');
+  });
+}
