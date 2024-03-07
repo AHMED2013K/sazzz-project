1b283c6a9d8bb573bf0007a100f7caba01936e55
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 11:20:28 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -7,4 +7,20 @@ function toggleModal() {
   modal.classList.toggle("slide-in");
 }
 
-// Supprimez la fonction submitForm que vous souhaitez éliminer
+// Function to submit the form
+function submitForm(event) {
+  event.preventDefault();
+@@ -34,8 +25,12 @@ function submitForm(event) {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Afficher un message de confirmation
+    alert('Application submitted successfully!');
+    toggleModal(); // Close modal after form submission
+    // Attendre 2 secondes avant de fermer la modal
+    setTimeout(function() {
+      toggleModal(); // Close modal after form submission
+    }, 2000);
+  })
+  .catch(error => {
+    console.error('There was an error!', error);
