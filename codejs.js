bf731bc59c3cecaf4dd5342f27bafb4d07ce8203
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 16:41:14 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -4,23 +4,37 @@ function submitApplication(event) {
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
 
-  // Envoyer les données du formulaire à Google Forms
-  fetch('https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', {
-    method: 'POST',
-    body: formData,
-  })
-  .then(response => {
-    if (response.ok) {
-      console.log('Form submitted successfully');
-      alert('Application submitted successfully!');
-      toggleModal();
-    } else {
-      console.error('Error submitting application:', response.statusText);
-      alert('Failed to submit application. Please try again later.');
-    }
-  })
-  .catch(error => {
-    console.error('Error submitting application:', error);
-    alert('Failed to submit application. Please try again later.');
+  // Convertir les données du formulaire en objet JSON
+  var jsonObject = {};
+  formData.forEach(function(value, key) {
+    jsonObject[key] = value;
   });
+
+  // Créer une requête XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  // Gérer la réponse
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // Vérifier si la soumission a réussi
+      if (xhr.status === 200) {
+        console.log('Form submitted successfully');
+        // Afficher un message de confirmation si nécessaire
+        alert('Application submitted successfully!');
+        // Fermer la modal après soumission du formulaire
+        toggleModal();
+      } else {
+        console.error('Error submitting application:', xhr.statusText);
+        alert('Failed to submit application. Please try again later.');
+      }
+    }
+  };
+
+  // Convertir les données du formulaire en format query string
+  var formDataQueryString = new URLSearchParams(jsonObject).toString();
+
+  // Envoyer la requête avec les données du formulaire
+  xhr.send(formDataQueryString);
 }
