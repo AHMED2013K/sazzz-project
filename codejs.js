c20b6e19453952f55d660ff8e243981385ca552e
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-07 16:15:07 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -4,26 +4,15 @@ function submitApplication(event) {
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
 
-  // Convertir les données du formulaire en objet JSON
-  var jsonObject = {};
-  formData.forEach(function(value, key) {
-    jsonObject[key] = value;
-  });
-
   // Envoyer les données du formulaire à Google Forms
   fetch('https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', {
     method: 'POST',
-    body: new URLSearchParams(jsonObject),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    body: formData,
   })
   .then(response => {
     if (response.ok) {
       console.log('Form submitted successfully');
-      // Afficher un message de confirmation si nécessaire
       alert('Application submitted successfully!');
-      // Fermer la modal après soumission du formulaire
       toggleModal();
     } else {
       console.error('Error submitting application:', response.statusText);
