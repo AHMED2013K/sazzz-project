9b1cd668fb1b2759ab7d8c26abe9e97dc56d3b31
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-08 11:41:02 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,34 +1,44 @@
-// Ajouter les en-têtes CORS
-function doGet(e) {
-  return handleResponse();
-}
+// Function to submit the application
+function submitApplication(event) {
+  event.preventDefault();
+  var form = document.getElementById("myForm");
+  var formData = new FormData(form);
 
-function doPost(e) {
-  try {
-    var parameters = e.parameter;
-
-    var full_name = parameters && parameters['Full Name'] ? parameters['Full Name'] : '';
-    var email = parameters && parameters['Email Address'] ? parameters['Email Address'] : '';
-    var phone_number = parameters && parameters['Phone Number'] ? parameters['Phone Number'] : '';
-    var country = parameters && parameters['Country of Residence'] ? parameters['Country of Residence'] : '';
-
-    // Votre code pour traiter les données du formulaire
-
-    // Répondre au client avec un code de succès
-    return ContentService.createTextOutput('Success').setMimeType(ContentService.MimeType.TEXT);
-  } catch (error) {
-    // En cas d'erreur, répondre au client avec un message d'erreur et enregistrer l'erreur dans les journaux
-    console.error(error);
-    return ContentService.createTextOutput('Error').setMimeType(ContentService.MimeType.TEXT);
-  }
-}
-
-function handleResponse() {
-  var response = ContentService.createTextOutput().setMimeType(ContentService.MimeType.JSON);
-  response.setHeaders({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type'
+  // Convertir les données du formulaire en objet JSON
+  var jsonObject = {};
+  formData.forEach(function(value, key) {
+    jsonObject[key] = value;
   });
-  return response;
+
+  // Afficher les données dans la console pour vérification
+  console.log("Form data:", jsonObject);
+
+  // Créer une requête XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://script.google.com/macros/s/AKfycbz6pzCL83lNkXHpp7FhlwDqlscx4EaIMLi-Gw5lWwanMFjQAEvIYypt1W9iMpgtqbmMdg/exec', true); // Remplacez par le nouvel URL de déploiement
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  // Gérer la réponse
+  xhr.onreadystatechange = function() {
+    console.log('Ready state:', xhr.readyState);
+    console.log('Status:', xhr.status);
+    console.log('Response:', xhr.responseText);
+
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // Vérifier si la soumission a réussi
+      if (xhr.status >= 200 && xhr.status < 300) {
+        console.log('Form submitted successfully');
+        toggleModal();
+      } else {
+        console.error('Error submitting application:', xhr.statusText);
+        alert('Failed to submit application. Please try again later.');
+      }
+    }
+  };
+
+  // Convertir les données du formulaire en format query string
+  var formDataQueryString = new URLSearchParams(jsonObject).toString();
+
+  // Envoyer la requête avec les données du formulaire
+  xhr.send(formDataQueryString);
 }
