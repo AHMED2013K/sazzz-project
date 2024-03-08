446065d4cb62f52f800fa523ade68620796fbb7a
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-08 17:18:21 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,8 +1,6 @@
 // Function to submit the application
 function submitApplication(event) {
   event.preventDefault();
-  console.log('Submitting form...'); // Ajout d'un message de débogage
-
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
 
