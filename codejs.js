902ec5dd0a2cf73ff02a3bcaa9f3ec4a6af984ab
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-11 11:05:47 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -4,17 +4,24 @@ function submitApplication(event) {
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
 
+  // Convertir les données du formulaire en format JSON
+  var jsonObject = {};
+  formData.forEach(function(value, key) {
+    jsonObject[key] = value;
+  });
+
   // Créer une requête XMLHttpRequest
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'submit.php', true); // Chemin vers le fichier PHP
+  xhr.open('POST', 'https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
   // Gérer la réponse
   xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      console.log('Ready state:', xhr.readyState);
-      console.log('Status:', xhr.status);
-      console.log('Response:', xhr.responseText);
+    console.log('Ready state:', xhr.readyState);
+    console.log('Status:', xhr.status);
+    console.log('Response:', xhr.responseText);
 
+    if (xhr.readyState === XMLHttpRequest.DONE) {
       // Vérifier si la soumission a réussi
       if (xhr.status >= 200 && xhr.status < 300) {
         console.log('Form submitted successfully');
@@ -26,8 +33,11 @@ function submitApplication(event) {
     }
   };
 
+  // Convertir les données du formulaire en format query string
+  var formDataQueryString = new URLSearchParams(jsonObject).toString();
+
   // Envoyer la requête avec les données du formulaire
-  xhr.send(formData);
+  xhr.send(formDataQueryString);
 }
 
 // Function to toggle modal
