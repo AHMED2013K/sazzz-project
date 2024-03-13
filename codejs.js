b67d0aa2ae329212afad3890da0f768003358121
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-13 13:03:09 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -1,4 +1,4 @@
-function submitApplication(event) {
+async function submitApplication(event) {
   event.preventDefault();
   var form = document.getElementById("myForm");
   var formData = new FormData(form);
@@ -9,32 +9,28 @@ function submitApplication(event) {
     jsonObject[key] = value;
   });
 
-  // Créer une requête XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  // Gérer la réponse
-  xhr.onreadystatechange = function() {
-    console.log('Ready state:', xhr.readyState);
-    console.log('Status:', xhr.status);
-    console.log('Response:', xhr.responseText);
-
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      // Vérifier si la soumission a réussi
-      if (xhr.status >= 200 && xhr.status < 300) {
-        console.log('Form submitted successfully');
-        toggleModal();
-      } else {
-        console.error('Error submitting application:', xhr.statusText);
-        alert('Failed to submit application. Please try again later.');
-      }
-    }
-  };
-
   // Convertir les données du formulaire en format query string
   var formDataQueryString = new URLSearchParams(jsonObject).toString();
 
   // Envoyer la requête avec les données du formulaire
-  xhr.send(formDataQueryString);
+  try {
+    const response = await fetch('https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formDataQueryString
+    });
+
+    if (response.ok) {
+      console.log('Form submitted successfully');
+      toggleModal();
+    } else {
+      console.error('Error submitting application:', response.statusText);
+      alert('Failed to submit application. Please try again later.');
+    }
+  } catch (error) {
+    console.error('Error submitting application:', error);
+    alert('Failed to submit application. Please try again later.');
+  }
 }
