ba898bf20b6495ce5b822678bffe8b74c9a3705d
AHMED2013K <162152543+AHMED2013K@users.noreply.github.com>
2024-03-14 10:39:16 +0100

    Update codejs.js

--- codejs.js
+++ codejs.js
@@ -14,8 +14,7 @@ async function submitApplication(event) {
 
   // Envoyer la requête avec les données du formulaire
   try {
-    const response = await fetch('https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', {
-      method: 'POST',
+    const response = await fetch('https://docs.google.com/forms/d/e/1FAIpQLSfdn2FnmzyIy0n-BjjzONVAwSu8RaUSqWIMz2qgCY9STS63Uw/formResponse', {method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
@@ -33,4 +32,29 @@ async function submitApplication(event) {
     console.error('Error submitting application:', error);
     alert('Failed to submit application. Please try again later.');
   }
+}
+
+// Fonction pour afficher et masquer le modal
+function toggleModal() {
+  const modal = document.getElementById('myModal');
+  modal.classList.toggle('active');
+}
+
+// Appeler la fonction animateText() après 1 seconde
+setTimeout(animateText, 1000);
+
+function animateText() {
+  const textElement = document.querySelector('.text-anim');
+  const text = textElement.textContent;
+  textElement.textContent = '';
+
+  let i = 0;
+  const interval = setInterval(() => {
+    if (i < text.length) {
+      textElement.textContent += text[i];
+      i++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 50);
 }
